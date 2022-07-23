3dc61539129de1cce008b2e2fe619a9cf728cd93
Licethd <liceth.dfe@gmail.com>
2022-07-23 01:05:06 -0400

    preuba3

--- tripulante/WebApi/src/main/java/WebApi.java
+++ tripulante/WebApi/src/main/java/WebApi.java
@@ -1,15 +1,15 @@
-
-import fourteam.http.Rest;
 import Controllers.TripulanteController;
+import fourteam.http.Rest;
 
-public class WebApi
-{
-    public static void AddControllers() {
-        Rest.addController(TripulanteController.class);
+public class WebApi {
 
-        Rest.start(8080);
-        Rest.createSwagger();
-    }
+	public static void AddControllers() {
+		Rest.addController(TripulanteController.class);
+
+		Rest.start(8080);
+		Rest.createSwagger();
+	}
+
 	public static void AddInfraestructure() {
 		// TODO Auto-generated method stub
 
