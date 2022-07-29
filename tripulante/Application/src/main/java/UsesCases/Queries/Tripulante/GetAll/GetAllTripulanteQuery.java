8e0bf26c071dfbf7a31ad39bea5545f830435e7e
Licethd <liceth.dfe@gmail.com>
2022-07-28 21:30:19 -0400

    limpiando

--- tripulante/Application/src/main/java/UsesCases/Queries/Tripulante/GetAll/GetAllTripulanteQuery.java
+++ tripulante/Application/src/main/java/UsesCases/Queries/Tripulante/GetAll/GetAllTripulanteQuery.java
@@ -1,13 +1,9 @@
 package UsesCases.Queries.Tripulante.GetAll;
 
-import java.util.UUID;
 import Dto.Tripulante.TripulanteDto;
 import fourteam.mediator.Request;
 
 public class GetAllTripulanteQuery implements Request<TripulanteDto> {
 
-    public GetAllTripulanteQuery() {
-    }
-
-
+	public GetAllTripulanteQuery() {}
 }
--- tripulante/Application/src/test/java/Dto/CargoDto_Test.java
+++ tripulante/Application/src/test/java/Dto/CargoDto_Test.java
@@ -1,27 +1,21 @@
 package Dto;
 
-import java.util.UUID;
-
+import Dto.Cargo.CargoDto;
 import org.junit.Assert;
 import org.junit.Test;
 
-import Dto.Cargo.CargoDto;
-
 public class CargoDto_Test {
 
-    @Test
-    public void CargoCheckPropertiesValid() {
+	@Test
+	public void CargoCheckPropertiesValid() {
+		String descripcion = "Asistente";
+		CargoDto cargo2 = new CargoDto();
+		CargoDto cargo = new CargoDto(null);
 
-        String descripcion = "Asistente";
-        CargoDto cargo2 = new CargoDto();
-        CargoDto cargo = new CargoDto(null);
+		Assert.assertNull(cargo.Descripcion);
 
-        Assert.assertNull(cargo.Descripcion);
+		cargo.setDescripcion(descripcion);
 
-        cargo.setDescripcion(descripcion);
-
-        Assert.assertEquals(descripcion, cargo.getDescripcion());
-     
-    }
-
+		Assert.assertEquals(descripcion, cargo.getDescripcion());
+	}
 }
