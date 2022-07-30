d36c74b91725a4e059477759de47bcb99a82c2f9
Licethd <liceth.dfe@gmail.com>
2022-07-30 11:06:44 -0400

    now

--- tripulante/Infraestructure/src/test/java/Context/MongoDB/WriteDbContext_Test.java
+++ tripulante/Infraestructure/src/test/java/Context/MongoDB/WriteDbContext_Test.java
@@ -1,28 +1,21 @@
 package Context.MongoDB;
 
+import fourteam.db.DbSet;
+import java.util.ArrayList;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import fourteam.db.DbSet;
-import fourteam.db.IDbSet.BooleanFunction;
-
-import static org.mockito.ArgumentMatchers.any;
-
-import java.util.ArrayList;
-
-
 public class WriteDbContext_Test {
-    DbSet dbSet = Mockito.mock(DbSet.class);
 
-    
-    @Test
-    public void test() {
-        WriteDbContext writeDbContext = new WriteDbContext();
-        writeDbContext.onModelCreating(new ArrayList<DbSet>());
-        writeDbContext.isConnected();
-        writeDbContext.Commit();
-        writeDbContext.Transaction();
-        writeDbContext.Rollback();
-    }
-    
-} 
+	DbSet dbSet = Mockito.mock(DbSet.class);
+
+	@Test
+	public void test() {
+		WriteDbContext writeDbContext = new WriteDbContext();
+		writeDbContext.onModelCreating(new ArrayList<DbSet>());
+		writeDbContext.isConnected();
+		writeDbContext.Commit();
+		writeDbContext.Transaction();
+		writeDbContext.Rollback();
+	}
+}
--- tripulante/SharedKernel/src/test/java/core/BussinessRulesValidateExption_Test.java
+++ tripulante/SharedKernel/src/test/java/core/BussinessRulesValidateExption_Test.java
@@ -1,20 +1,17 @@
 package core;
 
-import static org.mockito.Mockito.when;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class BussinessRulesValidateExption_Test {
 
-    @Test
-    public void constructor_accept() {
-
-        BussinessRuleValidateExeption rule = new BussinessRuleValidateExeption("detail");
-        Assert.assertNotNull(rule);
-        rule.getDetails();
-        rule.getBrokenRule();
-
-    }
+	@Test
+	public void constructor_accept() {
+		BussinessRuleValidateExeption rule = new BussinessRuleValidateExeption(
+			"detail"
+		);
+		Assert.assertNotNull(rule);
+		rule.getDetails();
+		rule.getBrokenRule();
+	}
 }
